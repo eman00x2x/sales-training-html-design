28bad4e7fc6b642b79c12aeb1deffe1f235f1c39
Eman Olivas <30310755+eman00x2x@users.noreply.github.com>
2024-04-24 13:24:59 +0800

    Add files via upload
    sample data added

---
+++ Cdn/js/data/accounts.js
@@ -0,0 +1,49 @@
+{
+	"data": [
+		{
+			"account_id": 1,
+			"organization_id": 1,
+			"username": "superusername",
+			"password": "9aa126e302832b2c95e29b11263b5e9f",
+			"account_type": "administrator",
+			"account_permission": [],
+			"registered_at": 1713923521
+		},
+		{
+			"account_id": 2,
+			"organization_id": 2,
+			"username": "regularuser1",
+			"password": "fb0fd131cffe9a9fa9f50d98860ed581",
+			"account_type": "subscriber",
+			"account_permission": [],
+			"registered_at": 1713923521
+		},
+		{
+			"account_id": 3,
+			"organization_id": 2,
+			"username": "regularuser2",
+			"password": "fb0fd131cffe9a9fa9f50d98860ed581",
+			"account_type": "subscriber",
+			"account_permission": [],
+			"registered_at": 1713923521
+		},
+		{
+			"account_id": 4,
+			"organization_id": 2,
+			"username": "regularuser3",
+			"password": "fb0fd131cffe9a9fa9f50d98860ed581",
+			"account_type": "subscriber",
+			"account_permission": [],
+			"registered_at": 1713923521
+		},
+		{
+			"account_id": 5,
+			"organization_id": 3,
+			"username": "regularuser4",
+			"password": "fb0fd131cffe9a9fa9f50d98860ed581",
+			"account_type": "subscriber",
+			"account_permission": [],
+			"registered_at": 1713923521
+		}
+	]
+}
