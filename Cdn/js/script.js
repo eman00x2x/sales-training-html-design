d83ff730cc23d9fc08861b156b0e40fe20d1d51a
Rogu <obsequio.rus@gmail.com>
2024-04-26 14:33:34 +0800

    Videos Create

--- Cdn/js/script.js
+++ Cdn/js/script.js
@@ -2,7 +2,7 @@ let data;
 
 const getModel = (url) => {
   $.getJSON(url, function (response) {
-	data = response.data;
+    data = response.data;
   });
 };
 
@@ -10,18 +10,18 @@ const convertDate = (dateData) => {
   let date = new Date(0);
   date.setUTCSeconds(dateData);
   return date.toLocaleDateString("en-US", {
-	year: "numeric",
-	month: "long",
-	day: "numeric",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
 };
 
 const niceTrim = (data, length) => {
-	if (data.length < length) {
-      return data;
-    } else {
-      return data.substring(0, length - 3) + "...";
-    }
+  if (data.length < length) {
+    return data;
+  } else {
+    return data.substring(0, length - 3) + "...";
+  }
 };
 
 $(document).on("click", ".btn-save", function (e) {
@@ -30,36 +30,36 @@ $(document).on("click", ".btn-save", function (e) {
   const form = $("#form");
 
   $(".btn-save").css({
-	cursor: "wait",
-	"pointer-events": "none",
+    cursor: "wait",
+    "pointer-events": "none",
   });
 
   $("#form :input").attr("readonly", true);
   $(".btn-save").hide();
 
   $(".response").html(
-	"<div class='bg-white p-3 mt-3 rounded'><div class='d-flex gap-3 align-items-center'><div class='loader'></div><p class='mb-0'>Processing, Please wait...</p></div></div>"
+    "<div class='bg-white p-3 mt-3 rounded'><div class='d-flex gap-3 align-items-center'><div class='loader'></div><p class='mb-0'>Processing, Please wait...</p></div></div>"
   );
   $("html, body").animate({ scrollTop: 0 }, "slow");
 
   setTimeout(function () {
-	if ((message = validateInput(form.serializeArray()))) {
-	  $(".response").html(message);
-	} else {
-	  $(".response").html(
-		"<p>Submitted but nothing happened! form data " +
-		  form.serialize() +
-		  " see <a href='../Cdn/js/script.js'>../Cdn/js/script.js</a></p>"
-	  );
-	}
-	  
-	  $(".btn-save").css({
-		  cursor: "pointer",
-		  "pointer-events": "auto",
-	  });
+    if ((message = validateInput(form.serializeArray()))) {
+      $(".response").html(message);
+    } else {
+      $(".response").html(
+        "<p>Submitted but nothing happened! form data " +
+          form.serialize() +
+          " see <a href='../Cdn/js/script.js'>../Cdn/js/script.js</a></p>"
+      );
+    }
 
-	  $(".btn-save").show();
-	  $("#form :input").removeAttr("readonly");
+    $(".btn-save").css({
+      cursor: "pointer",
+      "pointer-events": "auto",
+    });
+
+    $(".btn-save").show();
+    $("#form :input").removeAttr("readonly");
   }, 30);
 
   /* $.post(form.attr('action'), form.serialize(), function (data, status) {
