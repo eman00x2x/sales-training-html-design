379d4639ae39d0fa0c7a47e1c9e79dfeafe7b36f
jamiel00800 <jrsumeracruz@iskolarngbayan.pup.edu.ph>
2024-05-07 12:07:17 +0800

    organization by id done and manage video

--- Cdn/js/organization.users.list.js
+++ Cdn/js/organization.users.list.js
@@ -1,72 +1,66 @@
-$(document).ready(function () {
-    getOrganizationsData();
+        $(document).ready(function () {
+            getOrganizationsData();
 
-  });
-
-
-  $(document).on("keyup", '.search', function () {
-    var value = $(this).val().toLowerCase();
-    $(".userTable .data-container tr").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-});
+          });
+          $(document).on("keyup", '.search', function () {
+            var value = $(this).val().toLowerCase();
+            $(".userTable .data-container tr").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
 
 
-$(document).on('click', '.btn-add', function (e) {
-    window.location.href = "organization.user.create.html";
-});
+        $(document).on('click', '.btn-add', function (e) {
+            window.location.href = "organization.user.create.html";
+        });
 
-  function getOrganizationsData()
-  {
-    
+          function getOrganizationsData() {
+            $.getJSON("../Cdn/js/data/profiles.json", function (data) {
+                $.getJSON("../Cdn/js/data/accounts.json", function (account) {
+                    $.getJSON("../Cdn/js/data/organization.json", function (organization) {
 
-    $.getJSON("../Cdn/js/data/profiles.json", function (data) {
+                        const profileData = data.data;
+                        const accountData = account.data;
+                        const organizationData = organization.data;
 
+                        let params = new URL(document.location.toString()).searchParams;
+                        let id = params.get("id");
 
-      $.getJSON("../Cdn/js/data/accounts.json", function (account) { 
+                        let orgData = organizationData.find((item) => item.organization_id == id);
 
-        const profileData = data.data;
-        const accountData = account.data;
+                        if (orgData) {
+                            let orgId = orgData.organization_id;
+                            let mergedData = profileData.map(profile => {
+                                let accData = accountData.find(account => account.account_id === profile.account_id);
+                                return { ...profile, ...accData };
+                            });
+                            let filteredData = mergedData.filter(item => item.organization_id == orgId);
+                            const tableBody = $(".userTable tbody");
+                            filteredData.forEach((item) => {
+                                let fullName = `${item.name.prefix} ${item.name.firstname} ${item.name.lastname} ${item.name.suffix}`;
+                                let permanentAddress = item.address[0].permanent;
+                                let fullAddress = `${permanentAddress.region}, ${permanentAddress.province}, ${permanentAddress.municipality}, ${permanentAddress.barangay}`;
+                                let newRow = `
+                                    <tr>
+                                        <td>${item.profile_id}</td>
+                                        <td><img src="${item.profile_image}" alt="Profile Image" class="profileImg"></td>
+                                        <td>${fullName}</td>
+                                        <td>${fullAddress}</td>
+                                        <td>${item.account_type}</td>
+                                        <td>
+                                            <div class="d-flex flex-nowrap gap-1">
+                                                <a class="btn border-primary bg-body" href="organization.user.view.html?id=${item.profile_id}" > <i class="bi bi-eye"></i> View</a>
+                                                <a class="btn border-primary bg-body" href="organization.user.view.update.html?id=${item.profile_id}"> <i class="bi bi-pencil-square"></i> Edit</a> 
+                                                <a class="btn border-danger bg-body" href="organization.user.delete.html?id=${item.profile_id}" > <i class="bi bi-trash"></i> Delete</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                `;
+                                tableBody.append(newRow);
+                            });
+                        }
+                    });
+                });
+            });
+          }
 
-        for (let i = 0; i < profileData.length && i < accountData.length; i++) {
-          
-            let profData = profileData[i];
-            let accData = accountData[i];
-
-            const tableBody = $(".userTable tbody");
-
-             let fullName = `${profData.name.prefix} ${profData.name.firstname} ${profData.name.lastname} ${profData.name.suffix}`;
-             let permanentAddress = profData.address[0].permanent;
-             let fullAddress = `${permanentAddress.region}, ${permanentAddress.province}, ${permanentAddress.municipality}, ${permanentAddress.barangay}`;
-             let accType = "";
-                 if (profData.account_id === accData.account_id) {
-                     accType = accData.account_type;
-                 }
-           
-                 let newRow = `
-                 <tr>
-                     <td>${profData.profile_id}</td>
-                     <td><img src="${profData.profile_image}" alt="Profile Image" class="profileImg"></td>
-                     <td>${fullName}</td>
-                     <td>${fullAddress}</td>
-                     <td>${accType}</td>
-                     <td>
-                                     <div class="d-flex flex-nowrap gap-1">
-                                       <a class="btn border-primary bg-body" href="organization.user.view.html?id=${profData.profile_id}" > <i class="bi bi-eye"></i> View</a>
-               
-                                         <a class="btn border-primary bg-body"" href="organization.user.view.update.html?id=${profData.profile_id}"> <i class="bi bi-pencil-square"></i> Edit</a> 
-                                      <a class="btn border-danger bg-body" href="organization.user.delete.html?id=${profData.profile_id}" > <i class="bi bi-trash"></i> Delete</a>
-               
-                                     </div>
-                                   </td>
-                 </tr>
-               `;
-                 tableBody.append(newRow);
-
-
-
-        }
-   
-      });
-    });
-  }
