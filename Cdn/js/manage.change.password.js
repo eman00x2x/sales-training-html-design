ae948862c6f1f2cde054c51e9b0f5cc58abbf629
nieve0429 <rafhaelsacdalan@gmail.com>
2024-05-08 15:17:59 +0800

    Change password responsive + change icon for validation

--- Cdn/js/manage.change.password.js
+++ Cdn/js/manage.change.password.js
@@ -1,44 +1,48 @@
-function validateInput(input) {
+$(document).ready(function(){
+    function updatePasswordValidation() {
+        let newPasswordValue = document.getElementById('new-password').value;
+        let confirmPasswordValue = document.getElementById('confirm-password').value;
+        console.log("New Password:", newPasswordValue); // pan-check ng value
+        console.log("Confirm Password:", confirmPasswordValue); 
+        let iconElements = document.querySelectorAll('.password-requirement .bi');
 
-    let message = [];
+        if (newPasswordValue.length > 7) {
+            iconElements[0].className = 'bi bi-check-circle-fill fs-2 text-success eight-char';
 
-    let data = input.reduce(function (obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
+            if (/[a-z]/.test(newPasswordValue) && /[A-Z]/.test(newPasswordValue)) {
+                iconElements[1].className = 'bi bi-check-circle-fill fs-2 text-success upper-lower';
+            } else {
+                iconElements[1].className = 'bi bi-x-circle-fill fs-2 text-danger upper-lower';
+            }
 
-    let validator = validate({
-        thumbnail: data.thumbnail,
-        title: data.title,
-        subtitle: data.subtitle,
-        author: data.author,
-        category: data.category,
-        description: data.description,
-        group: data.group,
-        code: data.code,
-    }, {
-        current_password: {
-            presence: { allowEmpty: false },
-            type: "password",
-            length: { minimum: 8 }
-        },
-        subtitle: {
-            presence: { allowEmpty: false },
-            type: "password",
-            length: { minimum: 8 }
-        },
-        author: {
-            presence: { allowEmpty: false },
-            type: "password",
-            length: { minimum: 8 }
+            if (/\d/.test(newPasswordValue)) {
+                iconElements[2].className = 'bi bi-check-circle-fill fs-2 text-success one-num';
+            } else {
+                iconElements[2].className = 'bi bi-x-circle-fill fs-2 text-danger one-num';
+            }
+
+            if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(newPasswordValue)) {
+                iconElements[3].className = 'bi bi-check-circle-fill fs-2 text-success special-char';
+            } else {
+                iconElements[3].className = 'bi bi-x-circle-fill fs-2 text-danger special-char';
+            }
+
+            if (newPasswordValue === confirmPasswordValue) {
+                iconElements[4].className = 'bi bi-check-circle-fill fs-2 text-success match-password';
+            } else {
+                iconElements[4].className = 'bi bi-x-circle-fill fs-2 text-danger match-password';
+            }
+        } else {
+            iconElements[0].className = 'bi bi-x-circle-fill fs-2 text-danger eight-char';
+            iconElements[1].className = 'bi bi-x-circle-fill fs-2 text-danger upper-lower';
+            iconElements[2].className = 'bi bi-x-circle-fill fs-2 text-danger one-num';
+            iconElements[3].className = 'bi bi-x-circle-fill fs-2 text-danger special-char';
+            iconElements[4].className = 'bi bi-x-circle-fill fs-2 text-danger match-password';
         }
-    });
-
-    if (validator !== undefined) {
-        for (key in validator) {
-            message.push(validator[key]);
-        }
-        return message.join(", ");
     }
-    return false;
-}
+
+    document.getElementById('new-password').addEventListener('input', updatePasswordValidation);
+    document.getElementById('confirm-password').addEventListener('input', updatePasswordValidation);
+
+    updatePasswordValidation();
+});
