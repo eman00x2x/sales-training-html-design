06b6ab005491572470d58559076be8ebf287bcef
nieve0429 <rafhaelsacdalan@gmail.com>
2024-05-08 11:51:44 +0800

    Disabled inputs initially in profile details / skills. Enabled during edit and add, responsive

--- Cdn/js/manage.account.profile.details.js
+++ Cdn/js/manage.account.profile.details.js
@@ -1,63 +1,154 @@
+let mobile_ctr = 0;
+let educ_ctr = 0;
+
 $(document).ready(function () {
-    $(".mobile-add").css("display", "none")
-    $(".mobile-remove").css("display", "none")
+  $("#row-educ").removeClass();
 
-    $.getJSON('../Cdn/js/data/profiles.json', function (response) {
-        const formatDate = (epochTime) => {
-            let date = new Date(epochTime * 1000);
-            return date.toISOString().split('T')[0];
-        };
+  $.getJSON("../Cdn/js/data/profiles.json", function (response) {
+    const formatDate = (epochTime) => {
+      let date = new Date(epochTime * 1000);
+      return date.toISOString().split("T")[0];
+    };
 
-        id = getParams('id');
+    const urlParams = new URLSearchParams(window.location.search);
+    const userID = urlParams.get("id");
 
-        let profile = response.data.find(profile => profile.profile_id === parseInt(id));
+    let profile = response.data.find(
+      (profile) => profile.profile_id === parseInt(userID)
+    );
 
-        if (profile) {
-            $('#contact_number').val(profile.contact_number[0]);
-            $('#prefix').val(profile.name.prefix);
-            $('#firstname').val(profile.name.firstname);
-            $('#lastname').val(profile.name.lastname);
-            $('#suffix').val(profile.name.suffix);
-            $('#birthdate').val(formatDate(profile.birthday));
-            $('#facebook').val(profile.social_profile.facebook);
-            $('#linkedIn').val(profile.social_profile.linkedIn);
-            $('#perma-region').val(profile.address[0].permanent.region);
-            $('#perma-province').val(profile.address[0].permanent.province);
-            $('#perma-municipality').val(profile.address[0].permanent.municipality);
-            $('#perma-barangay').val(profile.address[0].permanent.barangay);
-            $('#region').val(profile.address[0].current.region);
-            $('#province').val(profile.address[0].current.province);
-            $('#municipality').val(profile.address[0].current.municipality);
-            $('#barangay').val(profile.address[0].current.barangay);
-            $('#school').val(profile.education[0].school);
-            $('#degree').val(profile.education[0].degree);
-            $('#graduated-on').val(formatDate(profile.education[0].graduated_at));
+    if (profile) {
+      $("#prefix").val(profile.name.prefix);
+      $("#firstname").val(profile.name.firstname);
+      $("#lastname").val(profile.name.lastname);
+      $("#suffix").val(profile.name.suffix);
+      $("#birthdate").val(formatDate(profile.birthday));
+      $("#facebook").val(profile.social_profile.facebook);
+      $("#linkedIn").val(profile.social_profile.linkedIn);
+      $("#perma-region").val(profile.address[0].permanent.region);
+      $("#perma-province").val(profile.address[0].permanent.province);
+      $("#perma-municipality").val(profile.address[0].permanent.municipality);
+      $("#perma-barangay").val(profile.address[0].permanent.barangay);
+      $("#region").val(profile.address[0].current.region);
+      $("#province").val(profile.address[0].current.province);
+      $("#municipality").val(profile.address[0].current.municipality);
+      $("#barangay").val(profile.address[0].current.barangay);
 
-        } else {
-            console.log("No profile found for the specified user ID.");
-        }
-    });
-})
+      profile.contact_number.map((item) => addMobileNumber(item));
+      mobile_ctr = profile.contact_number.length;
 
-$(document).on('click', '.btn-add', function () {
-    let new_chq_no = parseInt($('#total_mobile_num').val()) + 1;
-    let new_input = '<div id="row" class="d-flex justify-content-between align-items-center gap-1 mt-2">' +
-        '<input type="text" class="form-control" placeholder="Add Contact Number" aria-label="Recipient\'s username" aria-describedby="button-addon2">' +
-        '<button type="button" id="deleteRow" class="btn btn-remove btn-outline btn-outline-danger "><i class="bi bi-trash3"></i><span class="ms-2">Remove</span></button>' +
-        '</div>'
+      profile.education.map((item) => addEducation(item));
+      educ_ctr = profile.education.length;
 
-    $('#mobile_number').append(new_input);
-    $('#total_mobile_num').val(new_chq_no);
+    } else {
+      console.log("No profile found for the specified user ID.");
+    }
+  });
 });
 
-$(document).on('click', '#editButton', function () {
-    $(".mobile-add").css("display", "block")
-    $(".mobile-remove").css("display", "block")
-    $('#editButton').hide();
-    $('#saveButton').show();
+$(document).on("click", ".btn-add-mobile", function () {
+  addMobileNumber();
+  $("#row-mobile .input").prop('disabled', false)
+  $("#row-mobile .div-remove-mobile").removeClass("d-none");
+  mobile_ctr += 1;
+  console.log("nyaw");
 });
 
-$(document).on("click", "#deleteRow", function () {
-    $(this).parents("#row").remove();
-})
+$(document).on("click", ".btn-add-educ", function () {
+  addEducation();
+  $("#education #row-educ").addClass(
+    "bg-light shadow-sm border mt-2 px-2 rounded"
+  );
+  $("#row-educ .input").prop('disabled', false)
+  $("#row-educ .div-remove-educ").removeClass("d-none");
+  educ_ctr += 1;
+});
+
+$(".profile-content").on("click", ".editButton", function () {
+  $(".profile-content .input").prop('disabled', false)
+
+  // MOBILE NUMBER
+  $(".btn-add-mobile").removeClass("d-none");
+  $("#row-mobile .div-remove-mobile").removeClass("d-none");
+
+  // EDUCATION
+  $(".btn-add-educ").removeClass("d-none");
+
+  $("#education #row-educ").addClass(
+    "bg-light shadow-sm border mt-2 px-2 rounded"
+  );
+  $("#row-educ .div-remove-educ").removeClass("d-none");
+
+  $(".profile-content .editButton").hide();
+  $(".profile-content .saveButton").show();
+});
+
+$(document).on("click", "#deleteMobileRow", function () {
+  $(this).parents("#row-mobile").remove();
+});
+
+$(document).on("click", "#deleteEducRow", function () {
+  $(this).parents("#row-educ").remove();
+});
+
+function addMobileNumber(item = "") {
+  let input = `<div id="row-mobile" class="d-flex justify-content-between align-items-center gap-1 mb-2">
+                  <input type="text" class="input text-black form-control montserrat-regular" placeholder="Add Contact Number" disabled value="${item}">
+                  <button type="button" id="deleteMobileRow" class="btn div-remove-mobile btn-remove btn-outline btn-outline-danger d-none">
+                      <i class="bi bi-trash3"></i>
+                      <span class="ms-2">Remove</span>
+                  </button>
+              </div>`;
+
+  $(".mobile_number").append(input);
+}
+
+function addEducation(item = {}) {
+  let myDate;
+
+  if (item.hasOwnProperty("graduated_at")) {
+    let date = new Date(0);
+    date.setUTCSeconds(item.graduated_at);
+    myDate = date.toISOString().split("T")[0];
+  }
+
+  let input = `<div id='row-educ' class='mb-2'>
+                    <div class="row g-0">
+                        <label for="school" class="fs-4 text-secondary my-2 montserrat-regular">School</label>
+                        <div class="">
+                            <input type="text" name="school" id="school" class="input text-black form-control montserrat-regular" disabled value=${item.hasOwnProperty("school") > 0
+      ? item.school
+      : ""
+    }>
+                        </div>
+                    </div>
+                    <div class="row g-0 gap-2">
+                        <div class="col-md">
+                            <label for="degree" class="fs-4 text-secondary my-2 montserrat-regular">Degree</label>
+                            <input type="text" name="degree" id="degree" class="input text-black form-control montserrat-regular" disabled value=${item.hasOwnProperty("degree") > 0
+      ? item.degree
+      : ""
+    }
+                                >
+                        </div>
+                        <div class="col-md">
+                            <label for="graduated-on" class="fs-4 text-secondary my-2 montserrat-regular">Graduated
+                                on</label>
+                            <input type="date" name="graduated-on" id="graduated-on" class="input text-black form-control montserrat-regular" disabled value=${item.hasOwnProperty("graduated_at") > 0
+      ? myDate
+      : ""
+    }
+                                >
+                        </div>
+                    </div>
+                    <div class="text-center div-remove-educ d-none">
+                        <button type="button" id="deleteEducRow" class="btn my-3 btn-remove-educ btn-remove btn-outline btn-outline-danger ">
+                            <i class="bi bi-trash3"></i>
+                            <span class="ms-2">Remove</span>
+                        </button>
+                    </div>
+                </div>`;
+
+  $("#education").append(input);
+}
 
