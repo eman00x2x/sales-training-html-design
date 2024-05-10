4e993b81cf09d71eaa125b4eddb58e3b879fbef7
Rogu <116495454+rgrgogu@users.noreply.github.com>
2024-05-10 14:27:55 +0800

    Sort, Pagination and Search in Admin Organization Done

--- Cdn/js/admin.organization.list.js
+++ Cdn/js/admin.organization.list.js
@@ -1,107 +1,104 @@
-let pageNumber = 1, search = '', order = '', sortBy = '';
+let currentPage = 1, search = '', order = '', sortBy = '';
 
 $(document).ready(function () {
-    $.when(
-        $.getJSON("../Cdn/js/data/profiles.json"),
-        $.getJSON("../Cdn/js/data/accounts.json"),
-        $.getJSON("../Cdn/js/data/transactions.json")
-    ).done(function (data1, data2, data3) {
-        // MERGE PROFILE AND ACCOUNTS
-        let response = data1[0].data.map((a) =>
-            Object.assign(
-                a,
-                data2[0].data.find((b) => b.account_id == a.account_id)
-            )
-        );
-
-        let f = response.keys(response).find((key) => response[key].account_id == 1);
-        let transactions = data3[0].data.filter((item) => item.transaction_id == 1)[0]
-        let obj = { ...response[f], transaction: transactions };
-        console.log(obj)
-    })
-    getOrganizationsData(pageNumber, search, sortBy, order);
+    getOrganizationsData(sortBy, order);
 });
 
+// SEARCH
 $(document).on("keyup", '.search', function () {
     search = $(this).val().toLowerCase();
-
-    $(".organizations .data-container tr").filter(function () {
-        $(this).toggle($(this).find('td:eq(2)').text().toLowerCase().indexOf(search) > -1);
-    });
+    currentPage = 1
+    getOrganizationsData(sortBy, order);
 });
 
+// ASCENDING BUTTON
 $(document).on("click", '.btn-sort-asc', function () {
     $('.btn-sort-asc').addClass('active');
     $('.btn-sort-desc').removeClass('active');
     order = 'asc'
-
-    sortTable($('.organizations .data-container tr'), order, 'td:eq(2)')
+    getOrganizationsData(sortBy, order);
 });
 
+// DESCENDING BUTTON
 $(document).on("click", '.btn-sort-desc', function () {
     $('.btn-sort-desc').addClass('active');
     $('.btn-sort-asc').removeClass('active');
-
-    sortTable($('.organizations .data-container tr'), 'desc', 'td:eq(2)')
+    order = 'desc'
+    getOrganizationsData(sortBy, order);
 });
 
+// SORT BY ORGANIZATION NAME
 $(document).on("click", '.dropdown-org', function () {
-    let sort = $('.btn-sort-asc').hasClass("active") ? "asc" : "desc";
     $('.dropdown-org').addClass('active');
+    $('.dropdown-id').removeClass('active')
     $('.dropdown-created-date').removeClass('active');
-
-    sortTable($('.organizations .data-container tr'), sort, 'td:eq(2)')
+    sortBy = 'name'
+    getOrganizationsData(sortBy, order);
 });
 
-$(document).on("click", '.dropdown-created-date', function () {
-    let sort = $('.btn-sort-asc').hasClass("active") ? "asc" : "desc";
+// SORT BY ORGANIZATION ID
+$(document).on("click", '.dropdown-id', function () {
     $('.dropdown-org').removeClass('active');
-    $('.dropdown-created-date').addClass('active');
-    sortTable($('.organizations .data-container tr'), sort, 'td:eq(4)')
+    $('.dropdown-id').addClass('active')
+    $('.dropdown-created-date').removeClass('active');
+    sortBy = 'organization_id'
+    getOrganizationsData(sortBy, order);
 });
 
-function sortTable(classQuery, order, col) {
-    classQuery.sort(function (a, b) {
-        if (order === 'asc') {
-            return $(col, a).text().toLowerCase().localeCompare($(col, b).text().toLowerCase());
-        } else {
-            return $(col, b).text().toLowerCase().localeCompare($(col, a).text().toLowerCase());
-        }
-    }).appendTo($('.organizations .data-container'));
-}
+// SORT BY ORGANIZATION CREATED DATE
+$(document).on("click", '.dropdown-created-date', function () {
+    $('.dropdown-org').removeClass('active');
+    $('.dropdown-id').removeClass('active')
+    $('.dropdown-created-date').addClass('active');
+    sortBy = 'created_at'
+    getOrganizationsData(sortBy, order);
+});
 
+// PAGINATION
+$(document).on('click', '.btn-page', function (e) {
+    currentPage = $(this).data('num');
+    getOrganizationsData(sortBy, order);
+});
+
+// ADD ORGANIZATION
 $(document).on('click', '.btn-add', function (e) {
     window.location.href = "admin.organization.create.html";
 });
 
+// VIEW ORGANIZATION
 $(document).on('click', '.btn-view', function (e) {
     id = $(this).data('id');
     window.location.href = "admin.organization.view.html?id=" + id;
 });
 
+// EDIT ORGANIZATION
 $(document).on('click', '.btn-edit', function (e) {
     id = $(this).data('id');
     window.location.href = "admin.organization.update.html?id=" + id;
 });
 
+// DELETE ORGANIZATION
 $(document).on('click', '.btn-delete', function (e) {
     id = $(this).data('id');
     window.location.href = "admin.organization.delete.html?id=" + id;
 });
 
+// FUNCTION TO LOWERCASE ALL STRINGS
 function lowerCase(str) {
     return str.toLowerCase();
 }
 
-function isSearchQuery(query, data) {
-    const filter = query ? data.filter(item =>
-        lowerCase(item.name).includes(lowerCase(searchQuery)) ||
-        item.organization_id == query
+// FUNCTION TO RETURN DATA THAT IS SEARCHED, DEFAULT WILL BE THE RESPONSE DATA FROM JSON
+function isSearchQuery(data) {
+    const filter = search ? data.filter(item =>
+        lowerCase(item.name).includes(lowerCase(search)) ||
+        item.organization_id == search
     ) : data;
 
     return filter;
 }
 
+// SORT BY ORDER AND ITS PROPERTY
 function sortData(data, order, property) {
     switch (order) {
         case "asc":
@@ -109,36 +106,70 @@ function sortData(data, order, property) {
                 return data.sort((a, b) => a[property] - b[property])
             else if (property === "created_at")
                 return data.sort((a, b) => new Date(b[property]) - new Date(a[property]));
-            else
+            else if (property === "name")
                 return data.sort((a, b) => lowerCase(a[property]).localeCompare(lowerCase(b[property])));
+            else
+                return data.sort((a, b) => a.name.localeCompare(b.name));
         case "desc":
             if (property === "organization_id")
                 return data.sort((b, a) => a[property] - b[property])
             else if (property === "created_at")
                 return data.sort((a, b) => new Date(a[property]) - new Date(b[property]));
-            else
+            else if (property === "name")
                 return data.sort((b, a) => lowerCase(a[property]).localeCompare(lowerCase(b[property])));
+            else
+                return data.sort((b, a) => a.name.localeCompare(b.name));
         default:
             return data;
     }
 }
 
-function getOrganizationsData(currentPage, searchQuery, sortBy, order) {
+// UPDATE THE PAGE NUMBERS IN PAGE BUTTONS
+function updatePagination(totalPages) {
+    let paginationButtons = '';
+
+    if (totalPages > 0) {
+        paginationButtons += `
+        <button type="button" class="btn-page btn btn-outline-primary montserrat-semibold ${currentPage === 1 ? 'disabled' : ''}" data-num="${currentPage - 1}"">
+            <span class="d-none d-md-block">Previous</span>
+            <i class="bi bi-chevron-double-left d-block d-md-none"></i>
+        </button>`;
+
+        for (let i = 1; i <= totalPages; i++) {
+            const activeClass = i === currentPage ? 'active' : '';
+            paginationButtons += `<button type="button" class="btn-page btn btn-outline-primary ${activeClass}" data-num="${i}"">${i}</button>`;
+        }
+
+        paginationButtons += `
+        <button type="button" class="btn-page btn btn-outline-primary montserrat-semibold ${currentPage === totalPages ? 'disabled' : ''}" data-num="${currentPage + 1}">
+            <span class="d-none d-md-block">Next</span>
+            <i class="bi bi-chevron-double-right d-block d-md-none"></i>
+        </button>`;
+    } else
+        paginationButtons = '';
+
+    $('#page-numbers').html(totalPages > 0 ? `Showing ${currentPage} out of ${totalPages} pages` : 'Showing 0 out of 0 pages');
+    $('.page-buttons').html(paginationButtons);
+}
+
+// GET THE DATA
+function getOrganizationsData(sortBy, order) {
     const limit = 10;
-    const startIndex = (pageNumber - 1) * limit;
+    const startIndex = (currentPage - 1) * limit;
     const endIndex = startIndex + limit;
 
     let html = "";
 
     $.getJSON("../Cdn/js/data/organization.json", function (response) {
         const org = response.data;
-        const sortedData = sortData(isSearchQuery(searchQuery, org), order, sortBy);
+        const sortedData = sortData(isSearchQuery(org), order, sortBy);
         const totalItems = sortedData.length;
         const totalPages = Math.ceil(totalItems / limit);
-        const dataPages = sortedData.slice(startIndex, endIndex);
-
-        for (let i = 0; i < dataPages.length; i++) {
-            let data = dataPages[i];
+        updatePagination(totalPages);
+        const slicedData = sortedData.slice(startIndex, endIndex);
+        
+        for (let i = 0; i < slicedData.length; i++) {
+            let data = slicedData[i];
 
             html += "<tr>";
             html +=
