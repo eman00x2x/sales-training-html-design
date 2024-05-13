50aec3c264afa8a8a712b66a0b56e98783f01539
kycbulante <92903243+kycbulante@users.noreply.github.com>
2024-05-13 15:24:07 +0800

    read ebook update

--- Cdn/js/manage.read.ebook.js
+++ Cdn/js/manage.read.ebook.js
@@ -1,198 +1,77 @@
-function populateChapterDropdown(ebookId) {
+$(document).ready(function () {
+    const ebookId = getParams('ebook_id');
+    const ebookgroupId = getParams('ebook_group_id');
+    $(".ebooklist").attr("href", `manage.ebooks.html?id=${ebookgroupId}`);
+    $("#header").attr("href", `manage.ebooks.html?id=${ebookgroupId}`);
+
+    updateEbookDetails();
+    changefont();
+
+    // CHAPTERS
     $.getJSON('../Cdn/js/data/ebooks.chapters.json', function (chaptersData) {
         let chapters = chaptersData.data;
         const filteredChapters = chapters.filter(chapter => chapter.ebook_id == ebookId);
-        const chapterSelect = document.getElementById('chapterSelect');
+        const chapterSelect = $('#chapterSelect');
 
-        chapterSelect.innerHTML = '';
+        chapterSelect.empty();
 
         if (filteredChapters.length > 0) {
-            filteredChapters.forEach((chapter) => {
-                const option = document.createElement('option');
-                option.value = chapter.ebook_chapter_id;
-                option.textContent = chapter.chapter;
-                chapterSelect.appendChild(option);
+            $.each(filteredChapters, function (index, chapter) {
+                chapterSelect.append($('<option>', {
+                    value: chapter.ebook_chapter_id,
+                    text: chapter.chapter
+                }));
             });
 
-            const savedEbookId = sessionStorage.getItem('ebookId');
-            const savedChapters = JSON.parse(sessionStorage.getItem('savedChapters')) || {};
-            if (savedEbookId && savedEbookId === ebookId) {
-                const selectedChapterId = savedChapters[ebookId];
-                chapterSelect.value = selectedChapterId;
-                loadChapterContent(selectedChapterId);
-                updateButtonVisibility(selectedChapterId, filteredChapters);
-            } else {
-                sessionStorage.removeItem('selectedChapterId');
-                sessionStorage.removeItem('ebookId');
-                sessionStorage.setItem('ebookId', ebookId);
-                const selectedChapterId = filteredChapters[0].ebook_chapter_id;
-                sessionStorage.setItem('selectedChapterId', selectedChapterId);
-                chapterSelect.value = selectedChapterId;
-                loadChapterContent(selectedChapterId);
-                updateButtonVisibility(selectedChapterId, filteredChapters);
+            // Call the function to display the content of the first chapter initially
+            displayChapterContent(filteredChapters[0]);
 
-            }
-
-
-            chapterSelect.addEventListener('change', function () {
-                const selectedChapterId = parseInt(this.value); 
-                const selectedEbookId = sessionStorage.getItem('ebookId');
-                let savedChapters = JSON.parse(sessionStorage.getItem('savedChapters')) || {};
-                savedChapters[selectedEbookId] = selectedChapterId;
-                sessionStorage.setItem('savedChapters', JSON.stringify(savedChapters));
-                updateButtonVisibility(selectedChapterId, filteredChapters);
-                loadChapterContent(selectedChapterId);
-            });
-
-            document.getElementById('nextButton').addEventListener('click', function () {
-                const selectedIndex = chapterSelect.selectedIndex;
-                if (selectedIndex < filteredChapters.length - 1) {
-                    chapterSelect.selectedIndex = selectedIndex + 1;
-                    chapterSelect.dispatchEvent(new Event('change'));
-                }
-            });
-
-            document.getElementById('prevButton').addEventListener('click', function () {
-                const selectedIndex = chapterSelect.selectedIndex;
-                if (selectedIndex > 0) {
-                    chapterSelect.selectedIndex = selectedIndex - 1;
-                    chapterSelect.dispatchEvent(new Event('change'));
-                }
-            });
-
-            function updateButtonVisibility() {
-                const prevButton = document.getElementById('prevButton');
-                const nextButton = document.getElementById('nextButton');
-                const selectedIndex = chapterSelect.selectedIndex;
-
-                prevButton.style.display = selectedIndex === 0 ? 'none' : 'inline-block';
-
-                nextButton.textContent = selectedIndex === filteredChapters.length - 1 ? 'Take Exam' : 'Next';
-            }
-
-            function loadChapterContent(selectedChapterId) {
+            // Add event listener to the dropdown to update content when selection changes
+            chapterSelect.on('change', function () {
+                const selectedChapterId = $(this).val();
                 const selectedChapter = filteredChapters.find(chapter => chapter.ebook_chapter_id == selectedChapterId);
                 if (selectedChapter) {
-                    const contentArea = document.querySelector('.ebook-content');
-                    heading.textContent = selectedChapter.chapter;
-                    contentArea.innerHTML = `<p style="text-align: justify;text-indent: 2rem;">${selectedChapter.content}</p>`;
-                    sessionStorage.setItem('selectedChapterId', selectedChapterId);
+                    displayChapterContent(selectedChapter);
                 }
-            }
-
-        } else {
-            chapterSelect.innerHTML = '<option>No chapters found</option>';
+            });
         }
     });
-}
-
-function nextChapter() {
-    const chapterSelect = document.getElementById('chapterSelect');
-    const nextIndex = chapterSelect.selectedIndex + 1;
-    if (nextIndex < chapterSelect.options.length) {
-        chapterSelect.selectedIndex = nextIndex;
-        chapterSelect.dispatchEvent(new Event('change'));
-    } else {
-        window.location.href = 'take_exam.html';
-    }
-}
-
-function previousChapter() {
-    const chapterSelect = document.getElementById('chapterSelect');
-    const previousIndex = chapterSelect.selectedIndex - 1;
-    if (previousIndex >= 0) {
-        chapterSelect.selectedIndex = previousIndex;
-        chapterSelect.dispatchEvent(new Event('change'));
-    }
-}
-function updateButtonVisibility(selectedChapterId, filteredChapters) {
-    const prevButton = document.getElementById('prevButton');
-    const nextButton = document.getElementById('nextButton');
-
-    if (selectedChapterId === 1) {
-        prevButton.style.display = 'none';
-    } else {
-        prevButton.style.display = 'inline-block';
-    }
-
-    if (selectedChapterId === filteredChapters.length) {
-        nextButton.textContent = 'Take Exam';
-    } else {
-        nextButton.textContent = 'Next';
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const ebookId = getParams('ebook_id');
-    const ebookgroupId = getParams('ebook_group_id');
-    $(".ebooklist").attr("href",`manage.ebooks.html?id=`+ ebookgroupId);
-
-    populateChapterDropdown(ebookId);
-    for (let i = 0; i < sessionStorage.length; i++) {
-        const key = sessionStorage.key(i);
-        const value = sessionStorage.getItem(key);
-        console.log(`${key}: ${value}`);
-    }
-    
-
-    //font
-    let baseSize = 16;
-    let increment = 4;
-    let numberOfSizes = 5;
-
-    let select = document.getElementById('fontSizeSelector');
-    let textToResize = document.getElementById('textToResize');
-
-    for (let i = 0; i < numberOfSizes; i++) {
-        let size = baseSize + increment * i;
-        let option = document.createElement('option');
-        option.value = size;
-        option.textContent = size;
-        select.appendChild(option);
-    }
-
-    select.addEventListener('change', function () {
-        textToResize.style.fontSize = this.value + 'px';
-        heading.style.fontSize = parseInt(this.value) + 24 + 'px';
-    });
-
-    let savedFontSize = sessionStorage.getItem('fontSize');
-    if (savedFontSize) {
-        select.value = savedFontSize;
-        textToResize.style.fontSize = savedFontSize + 'px';
-    } else {
-        textToResize.style.fontSize = baseSize + 'px';
-    }
-
-    //color scheme
-    let savedColorScheme = sessionStorage.getItem('colorScheme');
-    if (savedColorScheme) {
-        document.querySelector(`input[name="radios-inline"][value="${savedColorScheme}"]`).checked = true;
-        document.body.classList.remove('light-scheme', 'gray-scheme', 'dark-scheme');
-        document.body.classList.add(`${savedColorScheme}-scheme`);
-    }
-
-    select.addEventListener('change', function () {
-        sessionStorage.setItem('fontSize', this.value);
-        textToResize.style.fontSize = this.value + 'px';
-        heading.style.fontSize = parseInt(this.value) + 24 + 'px';
-    });
-
-    document.querySelectorAll('input[name="radios-inline"]').forEach((radio) => {
-        radio.addEventListener('click', function () {
-            if (this.checked) {
-                const value = this.value;
-                document.body.classList.remove('light-scheme', 'gray-scheme', 'dark-scheme');
-                document.body.classList.add(`${value}-scheme`);
-                sessionStorage.setItem('colorScheme', value);
-            }
-        });
-    });
-
 });
 
+// Function to display chapter content
+function displayChapterContent(chapter) {
+    const contentArea = $('.ebook-content');
+    const heading = $('#heading');
+
+    heading.text(chapter.chapter);
+    contentArea.html(`<p style="text-align: justify; text-indent: 2rem;">${chapter.content}</p>`);
+    sessionStorage.setItem('selectedChapterId', chapter.ebook_chapter_id);
+}
 
 
+//DISPLAY EBOOK DETAILS
+function updateEbookDetails() {
+    const ebookId = getParams('ebook_id');
 
+    $.getJSON('../Cdn/js/data/ebooks.json', function (data) {
+        let response = data.data;
+        f = response.keys(response).find(key => response[key].ebook_id == ebookId);
 
+        $('#header').text(response[f].title);
+        $('.author').text(response[f].author);
+        $('.ebook-image').attr('src', response[f].thumbnail_image);
+    });
+}
+
+//FONT SIZE
+function changefont() {
+    const $fontSizeSelector = $('#fontSizeSelector');
+
+    for (let fontSize = 16; fontSize <= 32; fontSize += 2) {
+        $fontSizeSelector.append($('<option>', {
+            value: fontSize,
+            text: fontSize
+        }));
+    }
+}
 
