d2e9b05e05748d687bcba0b801a0e3dc64b3f84a
rgrgogu <obsequio.rus@gmail.com>
2024-06-04 15:53:28 +0800

    adjusted

---
+++ Cdn/js/website.registration.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.btn-signup').on('click', function(){
+        console.log("gege")
+        window.open('../../Website/email.verification.link.template.html', "_blank");
+        window.location.href = "../../Website/website.email.verification.html"
+    })
+});
+
+          
+          
