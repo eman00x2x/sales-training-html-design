3b86aafb3e7d228d66c8bf3b54c72262bb6b08dc
angelaadsn <camille.adison06@gmail.com>
2024-06-04 14:45:01 +0800

    my collection, sidebar adjustments

--- Cdn/js/manage.col.view.video.js
+++ Cdn/js/manage.col.view.video.js
@@ -29,4 +29,26 @@ $.getJSON('../Cdn/js/data/videos.json', function (videoData) {
         `;
     });
     $('.video-view').html(videoViewHtml);
+
+    let recommendedVideos = videosData.filter(video => video.video_id != videoId).slice(0, 3);
+    console.log(recommendedVideos)
+    let recommendedHtml = `
+                <h1 class="montserrat-semibold text-center">Recommended Videos</h1>`;
+
+    recommendedVideos.forEach(video => {
+        recommendedHtml += `
+                    <div class="card mt-3" data-ebook-id="${video.video_id}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" style="height:100%;">
+                    <!-- Photo -->
+                    <div class="img-container pt-2" style="height: 200px;">
+                        <img src="${video.thumbnail_image}" class="card-img" alt="Image"
+                            style="object-fit: contain; width: 100%; height: 100%;">
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title text-uppercase montserrat-semibold m-0">${video.title}</h3>
+                       
+                    </div>
+                </div>`;
+    });
+
+    $('.reco-videos').html(recommendedHtml);
 });
